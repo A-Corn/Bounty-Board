02eab536941ce95b747d4d3ba6205deb51b75426
Amir <amir.cornelius@gmail.com>
2022-09-19 20:36:48 -0400

    Added handleSubmit function

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,6 +1,29 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [err, setErr] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setErr("");
+        const postData = { email, password };
+        axios.post("". postData, {
+            withCredentials: true,
+        })
+        .then((res) => {
+            console.log(res);
+            navigate("/networking");
+        })
+        .catch((err) => console.log(err));
+    };
+
+    const navigate = useNavigate();
+
     return (
         <div className='container py-5 h-100'>
             <div className='row d-flex justify-content-center align-items-center h-100'>
@@ -10,22 +33,24 @@ const Login = () => {
                             <div className='col-lg-6'>
                                 <div className='card-body p-md-5 mx-md-4'>
                                     <h1 className='mt-1 mb-5 pb-1'> Bounty Board</h1>
-                                    <form>
+                                    <form onClick={handleSubmit}>
                                         <p> Please login to you account </p>
                                         <div className='form-outline mb-4'>
                                             <input type="email" id="form2Example11" class="form-control"
-                                                placeholder="Email address" />
+                                                placeholder="Email address" 
+                                                onChange={(e) => setEmail(e.target.value)}/>
                                         </div>
                                         <div class="form-outline mb-4">
                                             <input type="password" id="form2Example22" class="form-control"
-                                                placeholder="Enter your password"  />
+                                                placeholder="Enter your password"  
+                                                onChange={(e) => setPassword(e.target.value)}/>
                                         </div>
                                         <div class="text-center pt-1 mb-5 pb-1">
                                             <button className="btn btn-primary" type="button">Login</button>
                                         </div>
                                         <div class="d-flex align-items-center justify-content-center pb-4">
                                             <p class="mb-0 me-2">Don't have an account?</p>
-                                            <button type="button" class="btn btn-outline-danger">Create new</button>
+                                            <button class="btn btn-outline-danger" >Create new</button>
                                         </div>
                                     </form>
                                 </div>
