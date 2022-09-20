f99d96065acdc47669471408cbc68fcb47065f8c
Jeremy Hirschler <JJ.Hirschler@gmail.com>
2022-09-19 20:04:07 -0400

    added signup component, modified styling in app.css, connected to landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
 import LandingPage from "./components/LandingPage";
+import SignUp from "./components/SignUp";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />}/>
+          <Route path='/signup' element={<SignUp/>}/>
         </Routes>
       </BrowserRouter>
     </div>
